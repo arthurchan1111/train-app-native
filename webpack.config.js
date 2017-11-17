cd9e0b558a2b7ab5637eedb53a3dbb2201792b63
Arthur Chan <arthurchan1111@gmail.com>
2017-11-17 12:30:46 -0500

    Chore: Update webpack file to compile ts files with babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
 var path = require('path');
 
+ console.log(path);
 module.exports = {
-  entry: './index.js',
+  entry: {
+    app: './src/main',
+    polyfills: './src/polyfills'
+  },
+  resolve: {
+    extension: ['.ts','.js']
+  }
+  module:{
+    rule: [{
+      test: /\.ts$/,
+      loader: 'babel-loader',
+    },
+
+    }]
+  }
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
