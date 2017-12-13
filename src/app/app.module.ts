28610e8e817757bb66866b4d3926d959358cbe66
Arthur Chan <arthurchan1111@gmail.com>
2017-12-12 21:13:37 -0500

    Feat: Add signup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NavComponent } from './app/navbar/navbar.component';
 import { LoginComponent } from './app/login/login.component';
-
+import { RegisterComponent } from './app/register/register.component'
 import { BartService } from './app/bart_service/bart.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,http://puu.sh/yylcI/95676a8320.jpg
     HttpClientModule
   ],
   providers: [BartService],
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'sign-up',
+  templateUrl: './app/register/register.component.html',
+})
+
+export class RegisterComponent{
+  
+}
