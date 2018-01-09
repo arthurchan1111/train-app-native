f78277fcbbd50966969a12e94cd4965fe875ee10
Arthur Chan <arthurchan1111@gmail.com>
2018-01-08 22:53:16 -0500

    Feat: Change main files to support nativescript bootstrapping

---
+++ src/app/main.ts
@@ -0,0 +1,7 @@
+
+import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+
+import { AppModule } from "./app.module";
+
+
+platformNativeScriptDynamic().bootstrapModule(AppModule);
